0ebbc4749882e95553265b2de2bf89e55283862d
xjingx <46096090+xjingx@users.noreply.github.com>
2019-12-12 22:41:44 +0800

    注册输入框不能为空判断

--- magicube_server/src/main/resources/static/js/register.js
+++ magicube_server/src/main/resources/static/js/register.js
@@ -1,5 +1,54 @@
+var unempty1 = 0;
+var unempty2 = 0;
+var unempty3 = 0;
+var unempty4 = 0;
 $(function(){
+    $("#name").blur(function(){
+        var name = $("#name").val();
+        if (name == "") {
+            $("#nameError").show();
+        }else{
+            unempty1 = 1
+        }
+    })
+    $("#phonenumber").blur(function(){
+        var phonenumber = $("#phonenumber").val();
+        if(phonenumber == ""){
+            $("#phonenumberError").show();
+        }else{
+            unempty2 = 1
+        }
+    })
+    $("#email").blur(function(){
+        var email = $("#email").val();
+        if(email == ""){
+            $("#emailError").show();
+        }else{
+            unempty3 = 1
+        }
+    })
+    $("#password").blur(function(){
+        var password = $("#password").val();
+        if(password == ""){
+            $("#passwordError").show();
+        }else{
+            unempty4 = 1
+        }
+    })
     $("#regis").click(function(){
+    if(unempty1 != 1){
+        $("#nameError").show();
+        return ;
+    }else if(unempty2 != 1){
+        $("#phonenumberError").show();
+        return ;
+    }else if(unempty3 != 1){
+        $("#emailError").show();
+        return ;
+    }else if(unempty4 != 1){
+        $("#passwordError").show();
+        return ;
+    }else{
         var data = {
             "name": $("#name").val(),
             "phonenumber": $("#phonenumber").val(),
@@ -12,7 +61,7 @@ $(function(){
             data: data,
             dataType:'JSON',
             success:function(result){
-                if(result == "1"){
+                if(result != null){
                     alert("注册成功！");
                 }
             },
@@ -20,6 +69,7 @@ $(function(){
                 alert("注册失败！");
             }
         });
+    }
     })
 
 })
